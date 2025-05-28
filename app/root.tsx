33b7bd7cb9247843bf3b13e8771febfe3a1bad1f
kTz1 <catalin_simon90@yahoo.com>
2025-05-28 14:49:48 +0300

    add Syncfusion components and setup admin routes with layouts

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
+import { registerLicense } from "@syncfusion/ej2-base";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
@@ -22,6 +23,8 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
+
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
