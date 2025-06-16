91d028f454ded023c614059eaecddbc74d16777d
kTz1 <catalin_simon90@yahoo.com>
2025-06-16 11:50:04 +0300

    fix: bugs

--- app/routes/root/sign-in.tsx
+++ app/routes/root/sign-in.tsx
@@ -33,10 +33,7 @@ const SignIn = () => {
             <h2 className="p-28-semibold text-dark-100 text-center">
               Start Your Travel Journey
             </h2>
-            <p
-              className="p-18-regular text-center text-gray-100"
-              style={{ lineHeight: "1.75rem" }}
-            >
+            <p className="p-18-regular text-center">
               Sign in with Google to manage destinations, itineraries, and user
               activity with ease.
             </p>
